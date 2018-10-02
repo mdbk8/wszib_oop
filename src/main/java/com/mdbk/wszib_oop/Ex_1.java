abb4a2fa2a5b55e4348f79eebb23570f8716b1b3
mateuszdybas <mateuszdybas@gmail.com>
2018-10-02 16:42:07 +0200

    Exercise 1 skeleton

---
+++ src/main/java/com/mdbk/wszib_oop/Ex_1.java
@@ -0,0 +1,13 @@
+package com.mdbk.wszib_oop;
+
+/**
+ * Exercise 1:
+ * We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given two int values,
+ * return true if on eor the other is teen, but not both.
+ */
+public class Ex_1 {
+
+    public static boolean isTeen() {
+        return false;
+    }
+}
---
+++ src/test/java/com/mdbk/wszib_oop/Ex_1Test.java
@@ -0,0 +1,7 @@
+package com.mdbk.wszib_oop;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ex_1Test {
+
+}
