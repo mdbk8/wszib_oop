a361ab65a21e7cefb7d1222c35f4a0f8b59fa327
mateuszdybas <mateuszdybas@gmail.com>
2018-10-03 20:03:45 +0200

    Exercise 1 from week 1

---
+++ src/main/java/com/mdbk/wszib_oop/week_1/Ex_1.java
@@ -0,0 +1,28 @@
+package com.mdbk.wszib_oop.week_1;
+
+/**
+ * Exercise 1:
+ * We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given two int values,
+ * return true if one or the other is teen, but not both.
+ */
+public class Ex_1 {
+
+    private static int LOWER_BOUND = 13;
+    private static int UPPER_BOUND_INCLUSIVE = 19;
+
+    public static boolean isTeen(int firstNum, int secondNum) {
+        if (isWithinBounds(firstNum)) {
+            return isOutsideBounds(secondNum);
+        } else {
+            return isWithinBounds(secondNum);
+        }
+    }
+
+    private static boolean isOutsideBounds(int num) {
+        return num < LOWER_BOUND || num > UPPER_BOUND_INCLUSIVE;
+    }
+
+    private static boolean isWithinBounds(int num) {
+        return num >= LOWER_BOUND && num <= UPPER_BOUND_INCLUSIVE;
+    }
+}
--- src/test/java/com/mdbk/wszib_oop/Ex_1Test.java
+++
@@ -1,7 +0,0 @@
-package com.mdbk.wszib_oop;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class Ex_1Test {
-
-}
