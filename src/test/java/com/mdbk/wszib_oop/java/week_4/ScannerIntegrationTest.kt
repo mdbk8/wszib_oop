d1d72eede3d9bac6b3e9f4ab45be1dded2250695
mateuszdybas <mateuszdybas@gmail.com>
2018-12-19 16:35:46 +0100

    Integration tests for final logic

---
+++ src/test/java/com/mdbk/wszib_oop/java/week_4/ScannerIntegrationTest.kt
@@ -0,0 +1,71 @@
+package com.mdbk.wszib_oop.java.week_4
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class ScannerIntegrationTest {
+
+    private val fileReader: FileReader = mock()
+    private val accountNumberParser: AccountNumberParser = AccountNumberParser()
+    private val converter = ScannedNumberConverter()
+
+    private val tested by lazy { Scanner(fileReader, accountNumberParser, converter) }
+
+    @ParameterizedTest
+    @MethodSource("cardNumbers")
+    fun `returns list of account numbers`(args: ScannerArgs) {
+        whenever(fileReader.readFile()).thenReturn(args.inputLines)
+
+        val actual = tested.scanFile()
+
+        assertEquals(args.expectedCardNumbers, actual)
+    }
+
+    companion object {
+        @Suppress("unused")
+        @JvmStatic
+        fun cardNumbers(): Stream<ScannerArgs> = Stream.of(
+                ScannerArgs(
+                        inputLines = listOf(
+                                " _  _  _  _  _  _  _  _  _ ",
+                                "| || || || || || || || || |",
+                                "|_||_||_||_||_||_||_||_||_|",
+                                "                           ",
+                                "                           ",
+                                "  |  |  |  |  |  |  |  |  |",
+                                "  |  |  |  |  |  |  |  |  |",
+                                "                           ",
+                                " _  _  _  _  _  _  _  _  _ ",
+                                " _| _| _| _| _| _| _| _| _|",
+                                "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
+                                "                           "
+                        ),
+                        expectedCardNumbers = listOf("000000000", "111111111", "222222222")
+                ),
+                ScannerArgs(
+                        inputLines = listOf(
+                                " _  _  _  _  _  _  _  _  _ ",
+                                "| || || || || || || || || |",
+                                "|_||_||_||_||_||_||_||_||_|",
+                                "                           "
+                        ),
+                        expectedCardNumbers = listOf("000000000")
+                ),
+                ScannerArgs(
+                        inputLines = listOf(
+                                "    _  _     _  _  _  _  _ ",
+                                "  | _| _||_||_ |_   ||_||_|",
+                                "  ||_  _|  | _||_|  ||_| _|",
+                                "                           "
+                        ),
+                    expectedCardNumbers = listOf("123456789")
+                )
+        )
+    }
+
+    data class ScannerArgs(val inputLines: List<String>, val expectedCardNumbers: List<String>)
+}
